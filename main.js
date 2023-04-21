5e841e2d62d4b4bf38960c737cddd3c063b61c4f
Ciaran Brown <ciaranbrown@ciarans-air.home>
2023-04-21 14:20:16 +0100

    anchor scroll

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-/* Hamburger Navigation Bar */ 
+/* Hamburger Navigation Bar */
 
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const navBar = document.querySelector('.navbar');
 
-
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+document.querySelectorAll('.nav-link').forEach(n =>
+  n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
-}));
+  })
+);
+
+/* Anchor Scroll */
+
+document.querySelectorAll('a[href^="#]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
